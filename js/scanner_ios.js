8bdf26362533bae165f52f1d2b788c64496f97cd
marcosbillordo08-pixel <marcosbillordo08@gmail.com>
2026-06-30 20:32:39 -0300

    Update scanner_ios.js

--- js/scanner_ios.js
+++ js/scanner_ios.js
@@ -1,1 +1,78 @@
+const btnEscanear = document.getElementById("btnEscanear");
+const btnCerrar = document.getElementById("cerrarScanner");
+const modal = document.getElementById("modalScanner");
 
+let controls = null;
+let codeReader = null;
+
+btnEscanear.onclick = abrirScanner;
+btnCerrar.onclick = cerrarScanner;
+
+async function abrirScanner() {
+
+    modal.style.display = "flex";
+
+    codeReader = new ZXing.BrowserMultiFormatReader();
+
+    try {
+
+        const devices = await ZXing.BrowserCodeReader.listVideoInputDevices();
+
+        if (!devices.length) {
+            alert("No se encontró ninguna cámara.");
+            return;
+        }
+
+        let camera = devices[devices.length - 1];
+
+        controls = await codeReader.decodeFromVideoDevice(
+
+            camera.deviceId,
+
+            "reader",
+
+            (result, err) => {
+
+                if (result) {
+
+                    console.log("Código leído:");
+
+                    console.log(result.getText());
+
+                }
+
+            }
+
+        );
+
+    } catch (e) {
+
+        console.error(e);
+
+        alert("Error iniciando ZXing");
+
+    }
+
+}
+
+function cerrarScanner() {
+
+    if (controls) {
+
+        controls.stop();
+
+        controls = null;
+
+    }
+
+    if (codeReader) {
+
+        codeReader.reset();
+
+        codeReader = null;
+
+    }
+
+    modal.style.display = "none";
+
+}
