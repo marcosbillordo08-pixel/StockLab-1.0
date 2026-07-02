fba9752b093656f8039b81522d35e4e5b00f5dc8
marcosbillordo08-pixel <marcosbillordo08@gmail.com>
2026-07-01 22:43:16 -0300

    Update config-menu.js

--- js/config-menu.js
+++ js/config-menu.js
@@ -30,3 +30,28 @@ document.getElementById("btnImportar").addEventListener("click", function () {
     menuConfig.classList.remove("abierto");
 
 });
+
+const btnEstadisticas = document.getElementById("btnEstadisticas");
+const cerrarModalEstadisticas = document.getElementById("cerrarModalEstadisticas");
+const modalEstadisticas = document.getElementById("modalEstadisticas");
+
+if (btnEstadisticas) {
+    btnEstadisticas.addEventListener("click", function () {
+        menuConfig.classList.remove("abierto");
+        abrirModalEstadisticas();
+    });
+}
+
+if (cerrarModalEstadisticas) {
+    cerrarModalEstadisticas.addEventListener("click", function () {
+        cerrarModalEstadisticas();
+    });
+}
+
+if (modalEstadisticas) {
+    modalEstadisticas.addEventListener("click", function (e) {
+        if (e.target === modalEstadisticas) {
+            cerrarModalEstadisticas();
+        }
+    });
+}
